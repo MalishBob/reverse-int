d08f540389bc8854a3c7a263b09f50fb07083ab2
Azad <azad_63_mamedov@mail.ru>
2020-10-19 23:05:19 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n_str = n.toString();
+    n_str = n_str.replace('-', '');
+    n_str_new = '';
+    
+    for (let i = n_str.length-1; i >= 0 ; i--) {
+        n_str_new += n_str[i];
+    }
+    return parseInt(n_str_new);
 }
